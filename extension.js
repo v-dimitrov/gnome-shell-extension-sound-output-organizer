c5ceea67d1a67a00520c3648970117c05370d951
Valentin Dimitrov <valio86@gmail.com>
2026-07-05 22:18:43 +0300

    some spaces

--- extension.js
+++ extension.js
@@ -21,7 +21,7 @@
 import GLib from 'gi://GLib';
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
 export default class SoundOutputOrganizerExtension extends Extension {
     enable() {
